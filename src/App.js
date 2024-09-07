3eafa52ff4692a161677e75ff1273128ed6b8a8e
vaishnavi-yamjala <vaishnaviyamjala@gmail.com>
2024-09-07 23:31:58 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom';
+
+import Home from './components/Home';
+import WeatherPage from './components/WeatherPage';
 import './App.css';
+
+function Layout() {
+  return (
+    <div>
+      
+      <Outlet /> {/* This is where nested routes will be rendered */}
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/weather/:city" element={<WeatherPage/>} />
+        </Route>
+      </Routes>
+    </Router>
     </div>
   );
 }
